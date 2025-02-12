7f812c6bcc8420141951029d9f4f8bbc22672239
miguelChavezNava <miguel.271009@nv.ccsd.net>
2025-02-11 22:40:14 -0500

    Added a player with basic movement

--- Raging Gambler/Assets/Scripts/PlayerController.cs
+++ Raging Gambler/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed = 2f;
+    private Rigidbody2D rb;
+    private Vector2 direction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (direction * speed * Time.deltaTime));
     }
 }
